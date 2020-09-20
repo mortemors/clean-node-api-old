2464e7167e08303fe3af622648240095d04e02bd
Morte Mors <o-morte@hotmail.com>
2020-09-20 13:17:06 -0300

    refactor: move duplicated code to a helper method

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,19 +1,14 @@
 import { HttpRequest, HttpResponse } from '../protocols/http'
 import { MisssingParamError } from '../errors/missing-param-error'
+import { badRequest } from '../helpers/http-helper'
 
 export class SignUpController {
   handle (httpRequest: HttpRequest): HttpResponse {
     if (!httpRequest.body.name) {
-      return {
-        statusCode: 400,
-        body: new MisssingParamError('name')
-      }
+      return badRequest(new MisssingParamError('name'))
     }
     if (!httpRequest.body.email) {
-      return {
-        statusCode: 400,
-        body: new MisssingParamError('email')
-      }
+      return badRequest(new MisssingParamError('email'))
     }
   }
 }

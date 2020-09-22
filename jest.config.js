a7818ddcfaeebe86cc5026365fa2c0f9ba21fa74
Morte Mors <o-morte@hotmail.com>
2020-09-22 20:48:59 -0300

    chore: add jest config and scripts

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
-  coverageDirectory: 'covarage',
+  coverageDirectory: 'coverage',
   testEnvironment: 'node',
   transform: {
     '.+\\.ts$': 'ts-jest'
